ec08d74750c105bfa5a75a88219f16e097788970
Dawid Ruth <dawidolinp1@gmail.com>
2024-10-16 16:41:04 +0200

    Add project files.

---
+++ Coding Task The Kings Dawid Ruth/Monarchs.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Task_The_Kings_Dawid_Ruth
+{
+    public class Monarchs
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("nm")]
+        public string Name { get; set; }
+
+        [JsonProperty("cty")]
+        public string City { get; set; }
+
+        [JsonProperty("hse")]
+        public string House { get; set; }
+
+        [JsonProperty("yrs")]
+        public string Years { get; set; }
+
+        public int ReignStart { get; set; }
+        public int ReignEnd { get; set; }
+            
+        public void AssignReignYears()  //convert data to years 
+        {
+            if (!string.IsNullOrEmpty(Years))
+            {
+                //split string into two years 
+                var years = Years.Split('-');
+                //assign the first year to the starting year
+                if (int.TryParse(years[0], out int startYear))
+                {
+                    ReignStart = startYear;
+                }
+                //assign the second year to the end year, if it exists
+                if (years.Length > 1 && int.TryParse(years[1], out int endYear))
+                {
+                    ReignEnd = endYear;
+                }
+                else if (years.Length==1) //some rulers ruled for only one year
+                {
+                    ReignEnd = ReignStart;
+                }
+                //case for the current ruler 
+                else if (years.Length>1)
+                {
+                    ReignEnd = DateTime.Now.Year;
+                }
+            }
+        }
+
+        public int CountReignYears()
+        {
+            int yearCount = 0;
+            //both dates have to be bigger than 0 and end year > start year
+            if (ReignStart >= 0 && ReignEnd >= 0 && ReignEnd > ReignStart)
+            {
+                yearCount = ReignEnd - ReignStart;
+                //return yearCount;
+
+            }
+            //for years before year 0 but its no use in our case
+            //if (ReignStart < 0) 
+            //{
+            //    count = (ReignStart - ReignEnd)*(-1); 
+            //}
+            return yearCount;
+        }
+
+
+    }
+
+}
---
+++ Coding Task The Kings Dawid Ruth/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+namespace Coding_Task_The_Kings_Dawid_Ruth
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            var monarchs = await FetchMonarchs("https://gist.githubusercontent.com/christianpanton/10d65ccef9f29de3acd49d97ed423736/raw/b09563bc0c4b318132c7a738e679d4f984ef0048/kings");          
+            AnalyzeMonarchs(monarchs);
+        }
+        private static async Task<List<Monarchs>> FetchMonarchs(string url)
+        {
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync(url);
+            return JsonConvert.DeserializeObject<List<Monarchs>>(json);
+        }
+        private static void AnalyzeMonarchs(List<Monarchs> monarchs)
+        {
+            foreach (var monarch in monarchs)
+            {
+                monarch.AssignReignYears();
+                //Console.WriteLine($"Monarch: {monarch.Name}, ReignStart: {monarch.ReignStart}, ReignEnd: {monarch.ReignEnd}");
+
+            }
+            //1.
+            int monarchCount = monarchs.Count;
+            Console.WriteLine($"Number of monarchs is {monarchCount} ");
+
+            //2.
+            var longestReigningMonarch = monarchs
+                .Select(m => new { Monarch = m, ReignYears = m.CountReignYears() })
+                .OrderByDescending(m => m.ReignYears)
+                .FirstOrDefault();
+
+            Console.WriteLine($"Longest reigning monarch is {longestReigningMonarch.Monarch.Name} with {longestReigningMonarch.ReignYears} years of reign");
+            //3.
+            var houseReignYears = monarchs
+                .GroupBy(m => m.House)
+                .Select(group => new
+                {
+                    House = group.Key,
+                    TotalHouseReignYears = group.Sum(m => m.CountReignYears())
+                })
+                .OrderByDescending(h => h.TotalHouseReignYears)
+                .FirstOrDefault();
+            if (houseReignYears != null)
+            {
+                Console.WriteLine($"House with the most total reign years is {houseReignYears.House} with {houseReignYears.TotalHouseReignYears}");
+            }
+            //4.
+            var mostCommonFirstName = monarchs
+                .Select(m => m.Name.Split(' ')[0])
+                .GroupBy(name => name)
+                .Select(group => new
+                {
+                    firstName = group.Key,
+                    count = group.Count()
+                })
+                .OrderByDescending(x=>x.count)
+                .FirstOrDefault();
+            if(mostCommonFirstName != null)
+            {
+                Console.WriteLine($"Most common name of British monarch is {mostCommonFirstName.firstName} with the count of {mostCommonFirstName.count}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
